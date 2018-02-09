a91f5f9b839e8f1efea12fbd1bb765359d807c3f
tanyaalex91 <36223017+tanyaalex91@users.noreply.github.com>
2018-02-09 12:43:47 +0300

    Add files via upload

---
+++ src/ClassesAndObjects.java
@@ -0,0 +1,27 @@
+
+public class ClassesAndObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Person person1 = new Person();
+
+person1.name = "Tania";
+person1.size = 50;
+
+Person person2 = new Person();
+
+person1.name = "Sergey";
+person1.size = 45;
+
+System.out.println("person1.name");
+	}
+}
+	
+	
+	class Person{
+		
+		String name;
+		int size;
+	}
+	
+
