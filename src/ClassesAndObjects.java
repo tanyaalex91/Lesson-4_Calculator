4dec59941ec4f16667776786d8d61d8d5a482725
tanyaalex91 <36223017+tanyaalex91@users.noreply.github.com>
2018-02-09 13:06:23 +0300

    Add files via upload

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,22 +3,22 @@ public class ClassesAndObjects {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Person person1 = new Person();
+Pizza pizza1 = new Pizza();
 
-person1.name = "Tania";
-person1.size = 50;
+pizza1.name = "Pepperoni";
+pizza1.size = 50;
+System.out.println("Your order is" + " "+ pizza1.name + "." + "the size is" + " " + pizza1.size +  " " + "santimeters");
+Pizza pizza2 = new Pizza();
+System.out.println(" ");
+pizza2.name = "Four seasons";
+pizza2.size = 45;
+System.out.println("Your order is" + " "+ pizza2.name + "." + "the size is" + " " + pizza2.size +  " " + "santimeters");
 
-Person person2 = new Person();
-
-person1.name = "Sergey";
-person1.size = 45;
-
-System.out.println("person1.name");
 	}
 }
 	
 	
-	class Person{
+	class Pizza{
 		
 		String name;
 		int size;
